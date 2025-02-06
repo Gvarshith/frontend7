7371b298d7e4d13c297d66ab4949b00998272c16
Gvarshith <guvvalavarshith@gmail.com>
2025-02-06 15:23:23 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-document.getElementById("close").onclick = function() {
-    console.log("Close button clicked");
-    document.querySelector(".navbar.Hide").style.display = "none";
-}
+const menu = document.querySelector(".navbar.Hide");
+const himf = document.getElementById("him_f");
+const himc = document.getElementById("him_c");
+const drop = document.querySelector(".dropdown-menu");
+const dr = document.querySelector(".c");
 
+document.getElementById("close").onclick = function () {
+    menu.style.display = "none";
+    document.body.style.overflow = ""; // Reset scrolling
+    document.documentElement.style.overflow = ""; // Reset for <html>
+};
 
+document.getElementById("men").onclick = function () {
+    menu.style.display = "flex";
+    document.body.style.overflow = "hidden"; // Disable scrolling
+    document.documentElement.style.overflow = "hidden"; // Disable for <html>
+};
+
+document.getElementById("feature").onclick = function () {
+    himf.style.display = himf.style.display === "none" ? "block" : "none";
+};
+
+document.getElementById("company").onclick = function () {
+    himc.style.display = himc.style.display === "none" ? "block" : "none";
+};
+document.getElementById("feat").onclick = function () {
+    drop.style.display =  drop.style.display === "none" ? "block" : "none";
+};
+document.getElementById("com").onclick = function () {
+    dr.style.display =  dr.style.display === "none" ? "block" : "none";
+};
